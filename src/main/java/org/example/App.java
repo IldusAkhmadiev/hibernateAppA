871524587587f307fa94d75fe603dd9d3f09ee2c
Ildus <goodgoo12@mail.ru>
2023-03-28 14:31:22 +0300

    add hibernate and configure

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        // Подключаем проперти данные для работы с базой данных
+        Configuration configuration = new Configuration(); // hibernate.properties по умолчанию передается в конструктор
+        configuration.addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Person person = session.get(Person.class, 1);
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        session.getTransaction().commit();
+        sessionFactory.close();
+
     }
 }
