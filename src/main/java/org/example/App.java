195185ee06db2d8e8ca2bec80f80d3d57eed81f6
Ildus <goodgoo12@mail.ru>
2023-03-29 13:10:38 +0300

    add save person

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,18 +13,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        // Подключаем проперти данные для работы с базой данных
+//        System.out.println( "Hello World!" );
+//        // Подключаем проперти данные для работы с базой данных
+//        Configuration configuration = new Configuration(); // hibernate.properties по умолчанию передается в конструктор
+//        configuration.addAnnotatedClass(Person.class);
+//        SessionFactory sessionFactory = configuration.buildSessionFactory();
+//        Session session = sessionFactory.getCurrentSession();
+//        session.beginTransaction();
+//        Person person = session.get(Person.class, 1);
+//        System.out.println(person.getName());
+//        System.out.println(person.getAge());
+//        session.getTransaction().commit();
+//        sessionFactory.close();
+        System.out.println(savePerson());
+    }
+
+    public static Person savePerson()  {
         Configuration configuration = new Configuration(); // hibernate.properties по умолчанию передается в конструктор
         configuration.addAnnotatedClass(Person.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
-        Person person = session.get(Person.class, 1);
-        System.out.println(person.getName());
-        System.out.println(person.getAge());
+        Person person = new Person("Ali",33);
+        session.save(person);
+        System.out.println(person.getId());
         session.getTransaction().commit();
         sessionFactory.close();
-
+        return person;
     }
 }
