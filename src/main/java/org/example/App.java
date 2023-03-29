44251b5a2736a5991a19b363913366e4525ae354
Ildus <goodgoo12@mail.ru>
2023-03-29 13:52:20 +0300

    add update and delete

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,29 +9,24 @@ import org.hibernate.cfg.Configuration;
  * Hello world!
  *
  */
-public class App 
-{
+public class App  {
+    static Configuration configuration = null; // hibernate.properties по умолчанию передается в конструктор
+    static SessionFactory sessionFactory = null;
+    static {
+        configuration = new Configuration();
+        configuration.addAnnotatedClass(Person.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
-//        // Подключаем проперти данные для работы с базой данных
-//        Configuration configuration = new Configuration(); // hibernate.properties по умолчанию передается в конструктор
-//        configuration.addAnnotatedClass(Person.class);
-//        SessionFactory sessionFactory = configuration.buildSessionFactory();
-//        Session session = sessionFactory.getCurrentSession();
-//        session.beginTransaction();
-//        Person person = session.get(Person.class, 1);
-//        System.out.println(person.getName());
-//        System.out.println(person.getAge());
-//        session.getTransaction().commit();
-//        sessionFactory.close();
+
         System.out.println(savePerson());
+//        System.out.println(updatePerson());
     }
 
     public static Person savePerson()  {
-        Configuration configuration = new Configuration(); // hibernate.properties по умолчанию передается в конструктор
-        configuration.addAnnotatedClass(Person.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
         Person person = new Person("Ali",33);
@@ -39,6 +34,35 @@ public class App
         System.out.println(person.getId());
         session.getTransaction().commit();
         sessionFactory.close();
+        return person;
+    }
+
+    public static Person updatePerson() {
+        Person person = null;
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            person = session.get(Person.class, 2);
+            person.setName("Mua");
+//            person.setId(15);
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+        return person;
+    }
+
+    public static Person deletePerson() {
+        Person person = null;
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            person = session.get(Person.class, 2);
+            session.delete(person);
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
         return person;
     }
 }
